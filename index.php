46060ef0fa0c4ff2c4577c0ff95fcefa307318fa
Victor Mann <victorcmann@gmail.com>
2019-07-18 17:56:45 -0300

    formulario

--- index.php
+++ index.php
@@ -14,7 +14,37 @@
         <div class="logo"></div>
         <div class="row">
             <div class="col">
+                
                 <h1>Cadastro</h1>
+
+                <form>
+                    <div class="form-group">
+                        <label for="">Nome*</label>
+                        <input type="text" class="form-control" autofocus required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">E-mail*</label>
+                        <input type="email" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Telefone</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Celular</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <textarea type="text" class="form-control" placeholder="possui alguma experiencia com vendas, nos conte um pouco..." rows="5"></textarea>
+
+                    <div class="text-center mt-4">
+                        <button class="btn btn-lg btn-success">Cadastrar-se</button>
+
+                    </div>
+
+                    
+                </form>
+
             </div>
         </div>
     </div>
