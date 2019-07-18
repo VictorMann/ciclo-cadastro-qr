868ca9a2acecf37bccbb5becfabe652568a4e23f
Victor Mann <victorcmann@gmail.com>
2019-07-18 17:42:53 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style rel="stylesheet">
+    </style>
+    <title>Cadastro Ciclo Leiriense</title>
+</head>
+<body>
+    <div class="container">
+        <div class="logo"></div>
+        <div class="row">
+            <div class="col">
+                <h1>Cadastro</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
